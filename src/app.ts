cfdb54326520a98e67c5ab2483823644db928079
LWJerri <CLWJerri@yandex.ua>
2021-01-08 15:41:02 +0200

    Add localization manager boot

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import "source-map-support";
 import DiscordBoot from "./discord/shard";
+import { translate } from "./misc/lang";
 
+translate;
 DiscordBoot();
+
+process.on("uncaughtException", (err: any) => console.error(err));
+process.on("unhandledRejection", (err: any) => console.error(err));
